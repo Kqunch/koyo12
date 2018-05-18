7ac79c202d3f2c2c1b37e7c599e4e7109302c5ab
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-17 21:36:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,31 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'k!watchanime') {
-    	message.reply('https://www.animegg.org/popular-series');
-  	}
+    var msg = message.content.toLowerCase();
+    if (msg === 'k!onepiece') {
+      message.reply('https://www.animegg.org/series/one-piece);
+    } else if (msg === 'k!blend s') {
+      message.reply('https://www.animegg.org/series/blend-s);
+    } else if (msg === 'k!re:zero') {
+      message.reply('https://www.animegg.org/series/rezero-kara-hajimeru-isekai-seikatsu);
+    } else if (msg === 'k!help') {
+      message.reply('Use k! and type in your favorite anime! Watch online!');
+    } else if (msg === 'k!recommend') {
+      message.reply('blend s, SAO, One Piece, Re:Zero, etc. ');
+    } else if (msg === 'k!conan') {
+      message.reply('https://www.animegg.org/series/detectiveconan);
+    } else if (msg === 'k!naruto') {
+      message.reply('https://www.animegg.org/series/naruto');
+    } else if (msg === 'k!foodwars') {
+      message.reply('https://www.animegg.org/series/shokugeki-no-souma);
+    } else if (msg === 'k!dragonballz') {
+      message.reply('https://www.animegg.org/series/dragonball-z');
+    } else if (msg === 'k!pokemon') {
+      message.reply('https://www.animegg.org/search?search=pokemon');
+    } else if (msg === 'k!conan') {
+      message.reply('https://www.animegg.org/series/detectiveconan);
+    
+    }
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
