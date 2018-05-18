76722e93c99cc80051ca757cb43613c60d4a9044
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-17 21:22:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,12 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'k!life') {
     	message.reply('https://i.gyazo.com/9279aefaf06a54f51befe0af7328e292.gif');
-        
+});  
+    
 client.on('message', message => {
     if (message.content === 'k!anime') {
     	message.reply('https://www.animegg.org/popular-series');
-        
+});        
 client.on('message', message => {
     if (message.content === 'k!animerecommend') {
     	message.reply('https://www.animegg.org/series/blend-s');
