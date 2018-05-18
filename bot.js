6df38ee905c277c657dcdcf24ad423a979f3ba40
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-05-17 21:52:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,27 +7,18 @@ client.on('ready', () => {
 
 client.on('message', message => {
     var msg = message.content.toLowerCase();
-    if (msg === 'k!onepiece') {
-      message.reply('https://www.animegg.org/series/one-piece);
+    if (msg === 'k!assassination classroom') {
+      message.reply('https://www.watchcartoononline.io/anime/assassination-classroom-english-subbed');
     } else if (msg === 'k!blend s') {
-      message.reply('https://www.animegg.org/series/blend-s);
+      message.reply('https://https://www.watchcartoononline.io/anime/blend-s-english-subbed');
     } else if (msg === 'k!re:zero') {
-      message.reply('https://www.animegg.org/series/rezero-kara-hajimeru-isekai-seikatsu);
+      message.reply('https://www.watchcartoononline.io/anime/re-zero-kara-hajimeru-isekai-seikatsu-english-subbed');
     } else if (msg === 'k!help') {
-      message.reply('Use k! and type in your favorite anime! Watch online!');
-    } else if (msg === 'k!recommend') {
-      message.reply('blend s, SAO, One Piece, Re:Zero, etc. ');
-    } else if (msg === 'k!conan') {
-      message.reply('https://www.animegg.org/series/detectiveconan);
-    } else if (msg === 'k!naruto') {
-      message.reply('https://www.animegg.org/series/naruto');
-    } else if (msg === 'k!foodwars') {
-      message.reply('https://www.animegg.org/series/shokugeki-no-souma);
-    } else if (msg === 'k!dragonballz') {
-      message.reply('https://www.animegg.org/series/dragonball-z');
-    } else if (msg === 'k!pokemon') {
-      message.reply('https://www.animegg.org/search?search=pokemon');
-    
+      message.reply('To use this bot please enter the name of an anime with ^ in front of it and if we have that anime link it will');
+    } else if (msg === 'k!list') {
+      message.reply('assassination classroom, blend s, re:zero ');
+    } else if (msg === 'k!sao') {
+      message.reply('https://www.watchcartoononline.io/anime/sword-art-online');
     }
 });
 
